44cffcce3883970d20e9ddff574fb2afb94112da
Leon Li <byteflow777@gmail.com>
2023-08-08 22:34:27 +0800

    add: DependencyCheckType

---
+++ src/main/java/io/github/ileonli/winterframework/beans/factory/config/DependencyCheckType.java
@@ -0,0 +1,8 @@
+package io.github.ileonli.winterframework.beans.factory.config;
+
+public enum DependencyCheckType {
+    DEPENDENCY_CHECK_NONE,
+    DEPENDENCY_CHECK_OBJECTS,
+    DEPENDENCY_CHECK_SIMPLE,
+    DEPENDENCY_CHECK_ALL,
+}
